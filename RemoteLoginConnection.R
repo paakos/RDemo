09a9b5cae9816e7b9ee9175c2294919b486a21f1
paakos <paakos@hotmail.com>
2018-02-12 20:01:05 +0100

    update to include examples

--- RemoteLoginConnection.R
+++ RemoteLoginConnection.R
@@ -1,11 +1,9 @@
+# Conexion que genera promp user/pass
+
 remoteLogin("http://lon-hadoop-03.westeurope.cloudapp.azure.com:12800", 
             session = TRUE)
 
-x<-rnorm(1000)
-
-hist(x)
-
-remoteLogout()  
+# Conexion directa especificando server /usuario /clave
 
 remoteLogin(
   deployr_endpoint = "http://lon-hadoop-03.westeurope.cloudapp.azure.com:12800", 
@@ -14,5 +12,90 @@ remoteLogin(
   commandline = TRUE, 
   username = "admin", 
   password = "Pa55w.rd")
+
+
+exit
+
+
+
+
+
+#Ejemplos de uso local
+
+x<-rnorm(1000)
+
+hist(x)
+histogram(x)
+
+
+
+
+
+# Create a local data frame
+DF <- data.frame(sex = c("Male", "Male", "Female", "Male"),
+                 age = c(20, 20, 12, 10), score = 1.1:4.1)
+summary(DF)
+
+
+
+
+# get summary of sex variable
+library(RevoScaleR)
+library(dplyr)
+rxSummary(~ sex, DF)
+# obtain within sex-category statistics of the score variable
+rxSummary(score ~ sex, DF)
+
+
+# revisar que ejemplo hay cargados en la carpeta de samples 
+readPath <- rxGetOption("sampleDataDir")
+readPath
+
+
+
+Command <- paste("ls", readPath, sep="  -")
+mysamplefilepath <- paste(readPath, "CensusWorkers.xdf" ,sep="/") 
+
+mysamplefilepath
+
+Command
+getwd()
+
+system(Command,show.output.on.console=TRUE)
+list.dirs(path = readPath, full.names = TRUE, recursive = FALSE)
+
+
+
+
+library(dplyr)
+taxi_url <- "http://alizaidi.blob.core.windows.net/training/taxi_df.rds"
+taxi_df  <- readRDS(gzcon(url(taxi_url)))
+(taxi_df <- tbl_df(taxi_df))
+
+
+# Create a data source for the CensusWorkers.xdf file
+workerInfo <- RxXdfData(mysamplefilepath)
+
+# Perform functions that read from the CensusWorkers.xdf file
+head(workerInfo,100)
+rxSummary(~., workerInfo)
+
+
+remoteLogout() 
+
+
+
+your_name <- "pabloA"
+your_dir <- paste0('/usr/datadrive/', your_name)
+# File Path to your Data
+your_data <- file.path(your_dir, 'tripdata_2015.xdf')
+your_dir
+dir.create(your_dir)
+
+
+
+
+
+
 
 
